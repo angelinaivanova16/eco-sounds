1a536ac92c8f60dac0f0d19a2b399aa10baa2027
Angelina <angelinaivanova16@gmail.com>
2022-02-05 01:28:26 +0300

    feat: implement style selection for active element

--- eco-sounds/index.js
+++ eco-sounds/index.js
@@ -3,6 +3,7 @@ const audio = new Audio();
 let isPlay = false;
 const links = document.querySelector('.main-menu');
 const image = document.querySelector('.main-container');
+const birdsName = document.querySelectorAll("[data-bird]");
 
 
 function playAudio(event) {
@@ -28,20 +29,40 @@ function changeImage(event) {
 }
 
 
+
 function changeAudio(event) {
   if(!isPlay) {
     audio.src = `./assets/audio/${event.target.dataset.bird}.mp3`;
     audio.currentTime = 0;
     audio.play();
     isPlay = true;
-    toggleBtn()
+
+    toggleBtn() // для того, чтобы менялось изображение кнопки на pause
+
+    birdsName.forEach((el) => { // для добавления и удаления css класса active, чтобы активный элемент выделялся стилем
+      if (el == event.target){
+        el.classList.add('active')
+      } else {
+        el.classList.remove('active')
+      }
+    })
+
   } else {
     audio.src = `./assets/audio/${event.target.dataset.bird}.mp3`;
     audio.currentTime = 0;
     audio.play();
     isPlay = true;
+
+    birdsName.forEach((el) => {
+      if (el == event.target){
+        el.classList.add('active')
+      } else {
+        el.classList.remove('active')
+      }
+    })
   }
 }
+
 
 
 playBtn.addEventListener('click', toggleBtn);
